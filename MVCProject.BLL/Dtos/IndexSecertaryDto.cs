623e4337c5e9c0f0485f57e4fd960d130c09634c
Esraa Osama <eo6344848@gmail.com>
2025-09-12 19:02:20 +0300

    viewModels Secertary

---
+++ MVCProject.BLL/Dtos/IndexSecertaryDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCProject.BLL.Dtos
+{
+    public record IndexSecertaryDto(
+    string Id,
+   string Email,
+ string? Password,
+ string FirstName,
+ string LastName,
+ string OfficeNumber
+
+
+);
+    }
+
---
+++ MVCProject.BLL/Dtos/SecertaryDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCProject.BLL.Dtos
+{
+  
+        public record SecertaryDto(
+
+         string Email,
+       string Password,
+       string FirstName,
+       string LastName,
+       string OfficeNumber
+
+);
+    }
+}
